c56fb529fac2b64a262442eecf5e2f0324cea5da
Lucas Alves <lucasalvesofcc@gmail.com>
2024-07-02 17:01:54 -0300

    Update app.js

--- interacao/app.js
+++ interacao/app.js
@@ -7,10 +7,10 @@ switcher.addEventListener('click', function() {
 
     var className = document.body.className;
     if (className == "light-theme") {
-        this.textContent = "Escuro";
+        this.textContent = "Dark";
     }
     else {
-        this.textContent = "Claro";
+        this.textContent = "Light";
     }
 
     console.log('current class name: ' + className);
